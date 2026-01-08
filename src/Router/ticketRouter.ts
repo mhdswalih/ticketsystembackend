ad9b70e06e61efad167fc96cef0aa9fb91c7b1cb
mhdswalih <ctmswalihmc@gmail.com>
2026-01-08 17:50:49 +0530

    sfsdfs

--- src/Router/ticketRouter.ts
+++ src/Router/ticketRouter.ts
@@ -4,7 +4,6 @@ import { addTicket, getBooking } from '../Controller/ticketController'
 const ticketRouter = Router()
 
 ticketRouter.post('/confirm-booking', addTicket);
-ticketRouter.get('/get-bookings',getBooking)
-
+ticketRouter.get('/get-bookings', getBooking)
 
 export default ticketRouter
